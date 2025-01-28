66dc2838d459669ccb330907ae5180de09e9baf4
evie <eviezua@gmail.com>
2025-01-28 18:27:23 +0200

    Install and configure frontend

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const Encore = require('@symfony/webpack-encore');
+
+Encore
+    .setOutputPath('public/build/')
+    .setPublicPath('/build')
+    .addEntry('app', './assets/app.js')
+    .addStyleEntry('app_styles', './assets/styles/app.scss')
+    .enableVueLoader()
+    .enableSassLoader()
+    .splitEntryChunks()
+    .enableSingleRuntimeChunk()
+    .cleanupOutputBeforeBuild()
+    .enableBuildNotifications()
+    .enableSourceMaps(!Encore.isProduction())
+    .enableVersioning(Encore.isProduction())
+;
+
+module.exports = Encore.getWebpackConfig();
