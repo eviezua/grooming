b368b71011cbc818799421a945a146ab3213318f
evie <eviezua@gmail.com>
2025-06-07 02:35:33 +0300

    Add abstract Elasticsearch filter

---
+++ src/Filter/AbstractElasticSearchFilter.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Filter;
+
+use ApiPlatform\Doctrine\Orm\Filter\AbstractFilter;
+use ApiPlatform\Doctrine\Orm\Util\QueryNameGeneratorInterface;
+use ApiPlatform\Metadata\Operation;
+use Doctrine\ORM\QueryBuilder;
+use Doctrine\Persistence\ManagerRegistry;
+use Elastic\Elasticsearch\Client;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\Serializer\NameConverter\NameConverterInterface;
+
+abstract class AbstractElasticSearchFilter extends AbstractFilter
+{
+    protected Client $client;
+    protected string $indexName;
+    protected array $fields;
+
+    public function __construct(
+        ManagerRegistry $managerRegistry,
+        ?LoggerInterface $logger,
+        Client $client,
+        string $indexName,
+        array $fields,
+        ?NameConverterInterface $nameConverter = null
+    ) {
+        parent::__construct($managerRegistry, $logger, $nameConverter);
+        $this->client = $client;
+        $this->indexName = $indexName;
+        $this->fields = $fields;
+    }
+
+    protected function filterProperty(
+        string $property,
+        $value,
+        QueryBuilder $queryBuilder,
+        QueryNameGeneratorInterface $queryNameGenerator,
+        string $resourceClass,
+        ?Operation $operation = null,
+        array $context = []
+    ): void {
+        if ('search' !== $property || !$value) {
+            return;
+        }
+
+        $body = [
+            'query' => [
+                'multi_match' => [
+                    'query' => $value,
+                    'fields' => $this->fields,
+                ],
+            ],
+        ];
+
+        $result = $this->client->search([
+            'index' => $this->indexName,
+            'size' => 1000,
+            'body' => $body,
+        ]);
+
+        $ids = array_map(fn($hit) => $hit['_id'], $result['hits']['hits']);
+
+        if (count($ids) > 0) {
+            $alias = $queryBuilder->getRootAliases()[0];
+            $queryBuilder
+                ->andWhere("$alias.id IN (:elastic_ids)")
+                ->setParameter('elastic_ids', $ids);
+        } else {
+            $queryBuilder->andWhere('1 = 0');
+        }
+    }
+
+    public function getDescription(string $resourceClass): array
+    {
+        return [
+            'search' => [
+                'property' => 'search',
+                'type' => 'string',
+                'required' => false,
+            ],
+        ];
+    }
+}
